4394552e3ef42a94d45f74684e8b6d2ec6b27927
Carl <carl@MacBookAir.lan>
2026-05-25 22:34:34 +0800

    fix

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,10 @@
 use clap::Parser;
-use colored::*;
 use env_logger::{self};
 use log::{error, info};
-use skye::{Cli, read_repos_from_file, safe_write_to_file, sync_commits};
+use skye::{
+    Cli, build_ssh_builder, read_repos_from_file, safe_write_to_file, ssh_clone_repository,
+    sync_commits,
+};
 use std::process::exit;
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
@@ -12,58 +14,73 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
 
     env_logger::init();
 
-    let mut args = Cli::parse();
-    match &mut args.command {
+    let args = Cli::parse();
+    match args.command {
         skye::Commands::Sync {
-            source,
-            target,
-            output,
-            append,
-            ignore,
-            with_update_at,
-        } => {
-            if output.is_none() {
-                *output = Some(target.clone());
-            };
+            ref source,
+            ref target,
+            ref output,
+            append: _,
+            ignore: _,
+            with_update_at: _,
+        } => match output {
+            Some(output) => {
+                info!("source_path: {:?}", source);
+                info!("target_path: {:?}", &target);
+                info!("new_target_path: {:?}", output);
 
-            info!("source_path: {:?}", source);
-            info!("target_path: {:?}", &target);
-            info!("new_target_path: {:?}", output);
+                let source_repos = read_repos_from_file(&source)
+                    .expect(&format!("failed to read from file {:?}", &source));
 
-            let source_repos = read_repos_from_file(&source)
-                .expect(&format!("failed to read from file {:?}", &source));
+                let target_repos = read_repos_from_file(&target).unwrap_or_else(|_| {
+                    info!("not found valid file, create a new file: {:?}", target);
+                    Vec::new()
+                });
 
-            let target_repos = read_repos_from_file(&target).unwrap_or_else(|_| {
-                info!("not found valid file, create a new file: {:?}", target);
-                Vec::new()
-            });
+                info!("start sync {:?} -> {:?} to {:?}", source, target, output);
+                let updated_target = sync_commits(source_repos, target_repos, &args);
+                info!("safely write back...");
 
-            info!("start sync {:?} -> {:?} to {:?}", source, target, output);
-            let updated_target = sync_commits(source_repos, target_repos, &args);
-            info!("safely write back...");
-            match output.clone() {
-                None => {
-                    eprintln!("not a valid output path: None");
-                    exit(1);
-                }
-                Some(output) => {
-                    safe_write_to_file(output, &updated_target, &args)?;
-                }
+                safe_write_to_file(output, &updated_target, &args)?;
             }
-        }
+            None => {
+                eprintln!("not a valid output path: None");
+                exit(1);
+            }
+        },
         skye::Commands::Clone {
             source,
             clone_dir,
-            shallow,
-            ignore,
+            shallow: _,
+            ignore: _,
         } => {
             info!("clone projects");
             let source_repos = read_repos_from_file(&source)
                 .expect(&format!("failed to read from file {:?}", &source));
 
-            for repo in source_repos {
-                println!("repo: {:?}", repo.repo);
+            let ssh_builder = build_ssh_builder();
+
+            match ssh_builder {
+                Ok(mut builder) => {
+                    for mut repo in source_repos {
+                        println!("repo: {:?}", repo.repo);
+
+                        if repo.repo.starts_with("git@github.com") {
+                            repo.repo = repo.repo.replace(":", "/");
+                            repo.repo = format!("ssh://{}", repo.repo);
+                        }
+
+                        match ssh_clone_repository(&mut builder, &repo.repo, clone_dir.as_path()) {
+                            Ok(_) => continue,
+                            Err(e) => {
+                                error!("error to clone {}: {}", repo.repo, e);
+                            }
+                        }
+                    }
+                }
+                Err(_) => todo!(),
             }
+
             unimplemented!()
         }
     }
